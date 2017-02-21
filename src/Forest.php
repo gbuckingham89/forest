278644375850a605d83785bde309f27d2bc04048
George Buckingham <george@georgebuckingham.com>
2017-02-21 19:25:38 +0000

    Disable oEmbed in crap cleanup.

--- src/Forest.php
+++ src/Forest.php
@@ -69,6 +69,23 @@ class Forest
 	{
 		// Remove WP Generator text from the header
 		remove_action( 'wp_head', 'wp_generator' );
+
+		// Disable oEmbed
+		add_action( 'init', function() {
+
+			// Remove the REST API endpoint.
+			remove_action('rest_api_init', 'wp_oembed_register_route');
+
+			// Turn off oEmbed auto discovery.
+			// Don't filter oEmbed results.
+			remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+
+			// Remove oEmbed discovery links.
+			remove_action('wp_head', 'wp_oembed_add_discovery_links');
+
+			// Remove oEmbed-specific JavaScript from the front-end and back-end.
+			remove_action('wp_head', 'wp_oembed_add_host_js');
+		}, PHP_INT_MAX - 1 );
 	}
 
 	/**
